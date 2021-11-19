537a9e5afac26397ef16bd3c30de92e3d429b910
schandra <schandra@innominds.com>
2021-11-19 22:13:23 +0530

    Creating angular project with structure along with readme file

---
+++ eCommerceFrontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './global_components/signup/signup.component';
+import { LoginComponent } from './global_components/login/login.component';
+import { ChangepasswordComponent } from './global_components/changepassword/changepassword.component';
+import { ForgotpasswordComponent } from './global_components/forgotpassword/forgotpassword.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ChangepasswordComponent,
+    ForgotpasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
