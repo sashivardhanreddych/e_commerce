b57eb912fe7c2bc444113e19c3eec9690c224da7
schandra <schandra@innominds.com>
2021-11-20 23:13:20 +0530

    added angular error handling by using reactive form module and updated code

--- eCommerceFrontend/src/app/app.module.ts
+++ eCommerceFrontend/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Imported components from Internal dependencies
 import { AppComponent } from './app.component';
 import { SignupComponent } from './global_components/signup/signup.component';
 import { LoginComponent } from './global_components/login/login.component';
 import { ChangepasswordComponent } from './global_components/changepassword/changepassword.component';
 import { ForgotpasswordComponent } from './global_components/forgotpassword/forgotpassword.component';
-import { PipesPipe } from './pipes.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
-import { HeaderComponent } from './user_dashboard/header/header.component';
+import { HeaderComponent } from './user-dashboard/header/header.component';
+
+// Imports from the primeNg
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from "primeng/divider";
 
 @NgModule({
   declarations: [
@@ -19,13 +25,18 @@ import { HeaderComponent } from './user_dashboard/header/header.component';
     LoginComponent,
     ChangepasswordComponent,
     ForgotpasswordComponent,
-    PipesPipe,
     FilterPipe,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    //primeNg modules
+    PasswordModule,
+    DividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
